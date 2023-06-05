927b6ad803e840e6379fda0ea0992885133341cb
govinduabhiprakash <130291665+govinduabhiprakash@users.noreply.github.com>
2023-06-05 22:13:22 +0530

    added few things

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Being Zero")
+var data1={image : "abc.jpg",name : " G ABHIPRAKASH", designation:"Intern"};
+var data2={image : "edf.jpg",name : "ABHI", designation:"Software Devoloper"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
+}
